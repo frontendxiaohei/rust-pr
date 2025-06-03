9eb2c7e535be2caf3ffe0f33db185c352ce801f9
a coder <2112778581@qq.com>
2025-06-03 18:14:55 +0800

    feat(trait): trait

--- examples/trait7.rs
+++ examples/trait7.rs
@@ -1,6 +1,33 @@
+trait Animal {
+    fn name(&self) -> &str;
+}
 
+struct Dog;
 
+impl Animal for Dog {
+    fn name(&self) -> &str {
+        "Dog"
+    }
+}
+
+struct Cat;
+
+impl Animal for Cat {
+    fn name(&self) -> &str {
+        "Cat"
+    }
+}
+
+// 修改函数接受 &dyn Animal
+fn print_animal_name(animal: &dyn Animal) {
+    println!("{}", animal.name());
+}
 
 fn main() {
+    let animals: Vec<Box<dyn Animal>> = vec![Box::new(Dog), Box::new(Cat)];
 
+    for animal in animals {
+        // 通过解引用并取引用传递
+        print_animal_name(&*animal);
+    }
 }
